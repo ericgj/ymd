5d7c172b2b89b76b4af25e35e629e75742c0d3f6
Eric Gjertsen <ericgj72@gmail.com>
2014-12-24 13:48:30 -0500

    add ymd.parse, ymd.parse.utc
    fix missing dev dependency (testutil 0.6.x) for testing

--- test/ymd.test.js
+++ test/ymd.test.js
@@ -10,3 +10,24 @@ describe('+ ymd', function() {
     EQ (ymd(new Date('2013-03-05 4:43 PM')), '2013-03-05')
   })
 })
+
+describe('+ ymd.parse', function() {
+
+  it('should return the UTC date from string', function(){
+    EQ (ymd.parse.utc('2013-03-05').valueOf, (Date.UTC(2013,3,5)).valueOf);  
+  })
+
+  it('should return the local date from string', function(){
+    EQ (ymd.parse('2013-03-05').valueOf, (new Date(2013,3,5)).valueOf);  
+  })
+
+  it('should return the local date for yyyy-m-d string', function(){
+    EQ (ymd.parse('2013-3-5').valueOf, (new Date(2013,3,5)).valueOf);  
+  })
+
+  it('should throw error if date format not yyyy-mm-dd', function(done){
+    try      { ymd.parse('03/05/2013') }
+    catch (e){ return done() }
+    return done('failed');
+  })
+})
